58f53dbafc602c748fcc000f947f68d1a5ae2643
Stephany Vlz <5323100091@correcaminos.uts.edu.mx>
2024-10-07 14:39:25 -0700

    Add files via upload
    Subida de archivos

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Calculadora_SAVR.Services;
+using Calculadora_SAVR.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculadora_SAVR
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Calculadora_SAVR.ViewModels;
+using Calculadora_SAVR.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Calculadora_SAVR
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
